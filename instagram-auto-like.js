d9c601d664b6ead4efe094ab2f1e9e054171fe4a
Giuseppe Rignanese <44514517+giuseppegg02@users.noreply.github.com>
2023-04-09 12:33:39 +0200

    more easy to understand

--- instagram-auto-like.js
+++ instagram-auto-like.js
@@ -1,13 +1,14 @@
 var instaLike = document.getElementsByClassName("dCJp8 afkep coreSpriteHeartOpen _0mzm-");
-var noOfPosts = 10;// change the number to no of posts you want to like on your timeline
-for (var i = 0; i < noOfPosts; i++) {
+var PostToLike = 10;// change the number to no of posts you want to like on your timeline
+for (var i = 0; i < PostToLike; i++) {
     // note if the post is already been liked then the code will unlike it   
     instaLike[i].click();
 }
 //-------------------------------------------- if name matches like the post------------------------
 var getNameList = document.getElementsByClassName('FPmhX notranslate nJAzx');
 // will like all the posts of a person from the first 15 posts on your timeline
-for (var i = 0; i < 15; i++) {
+var PostToLike2 = 10;// change the number to no of posts you want to like on your timeline
+for (var i = 0; i < PostToLike2; i++) {
     var nameList = getNameList[i].text;
     var nameOfPerson = " test ";// replace 'test' with the name of the person you want to like the post of
     if (nameList == nameOfPerson) {
